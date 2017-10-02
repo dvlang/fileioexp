ebadb0ad620a4c0231b54a8bc02bab632b9e80f9
dvlang <daniel.v.lang@gmail.com>
2017-10-01 21:06:44 -0500

    working pars on semicolon

--- fileioexp/fileioexp.cpp
+++ fileioexp/fileioexp.cpp
@@ -12,7 +12,6 @@ using namespace std;
 char semicolin;
 int age;
 char name;
-//char *tokenPtr;
 string wholeentry;
 string accountNumber;
 string accountFirstname;
@@ -33,46 +32,49 @@ int main()
 {
 	accountStruct Person1;
 
-	cout << "FILE IO TESTING" << endl;
+	cout << "WELCOME TO THE ACCOUNTS MANAGER" << endl<<endl;
+	cout << "Please select from one of the following options:" << endl;
+	cout << "Press ""I"" to insert a new account record" << endl;
+	cout << "Press ""M"" to modify and existing record" << endl;
+	cout << "Press ""I"" to view all account records" << endl;
+	cout << "Press ""Q"" to quit" << endl;
+	cout << "Entry -> ";
 
 	ifstream in_file;
 	in_file.open("input.dat");
 
 	if (in_file.fail()) { cout << "ERROR: NO SUCH FILE" << endl; return 0; }	//check for failure when opening
 
-	/*while (getline(in_file, wholeentry))
-	{
 
-	}*/
 	getline(in_file, wholeentry);
 
+	//BEGIN LINE PARSING
 	int i = 0;
-	while (!isspace(wholeentry[i])) { i++; }
-	accountNumber = wholeentry.substr(0, i-1);
-
-	cout << "i = " << i << endl;
-
-	int j = i+1;
-	i++;
-	int y;
-	for (y = 0; y < 2; y++) {
-		while (!isspace(wholeentry[i])) { i++; }
-	}
-	accountname = wholeentry.substr(j, i-2);
-	cout << "i = " << i << endl;
-	
-	j = j+i;
-	
-	while (!isspace(wholeentry[i])) { i++; }
-	accountValue = wholeentry.substr(j, i - 2);
-	
-	Person1.name_Owner = accountname;
+	while (wholeentry.compare(i, 1, ";") != 0) { i++;}
+	accountNumber = wholeentry.substr(0, i);
 	tmpstring.str(accountNumber);
 	tmpstring >> Person1.account_Number;
 
+	i+=2;	//move i index off ";" and space
+	int j = i;	//j is placeholder to beginning of next entry
+	while (wholeentry.compare(i, 1, ";") != 0) {  i++; }
+	accountname = wholeentry.substr(j, i-j); 
+	Person1.name_Owner = accountname;
+
+	i += 2;	//move i index off ";" and space
+	j = i;	//j is placeholder to beginning of next entry
+	while (wholeentry.compare(i, 1, ";") != 0) { i++; }
+	accountValue = wholeentry.substr(j, i-j);
 	tmpstring2.str(accountValue);
-	
 	tmpstring2 >> Person1.amount_Avail;
+	// END LINE PARSING	
+	
+	
+	
+
+
+
+
 
 
 	cout << "Account Number: "<< Person1.account_Number << endl;
@@ -80,17 +82,7 @@ int main()
 	cout << "Account Total: " << Person1.amount_Avail << endl;
 		in_file.close();
 
-		cout << wholeentry << endl;
-		//working string compare
-		string tmpstring3 = ";";
-		//if (tmpstring3.compare(";")!=0) {
-		if (wholeentry.compare(28,1,";") != 0) {
-			cout << "it is a NOT semicolin"<<endl;
-		}
-		else {
-			cout << "it is a semicolin"<<endl;
-		}
-		//end string compare
+
 
 	return 0;
 }
